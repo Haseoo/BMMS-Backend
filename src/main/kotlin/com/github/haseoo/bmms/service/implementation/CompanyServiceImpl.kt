d482bcf95e082fc558a78f670b42d689aabe778a
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2022-07-14 21:13:14 +0200

    Fix class name

--- src/main/kotlin/com/github/haseoo/bmms/service/implementation/CompanyServiceImpl.kt
+++ src/main/kotlin/com/github/haseoo/bmms/service/implementation/CompanyServiceImpl.kt
@@ -1,13 +1,13 @@
 package com.github.haseoo.bmms.service.implementation
 
 import com.github.haseoo.bmms.domain.Company
-import com.github.haseoo.bmms.repository.adapter.CompanyRepository
+import com.github.haseoo.bmms.repository.adapter.CompanyRepositoryPort
 import com.github.haseoo.bmms.service.CompanyService
 import org.springframework.stereotype.Service
 
 @Service
 private class CompanyServiceImpl(
-    private val companyRepository: CompanyRepository
+    private val companyRepository: CompanyRepositoryPort
 ) : CompanyService {
     override fun getAll(): Collection<Company> = companyRepository.findAll()
 }
